fd4aada1986205df72ca570ab6334c8317d0bdc2
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-02-01 12:13:11 +0530

    Added login page

---
+++ src/pages/login.js
@@ -0,0 +1,15 @@
+import styles from '@/styles/Login.module.css'
+export default function Login() {
+    return <div className={styles.main}>
+        <div className={styles.title}>
+            Login
+        </div>
+        <div className={styles.loginContainer}>
+            <div className={styles.imageContainer}/>
+            <hr/>
+            <div className={styles.loginForm}>
+
+            </div>
+        </div>
+    </div>
+}
