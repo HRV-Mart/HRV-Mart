bbf284aa4cc4da6de5913357a9350f65bc229989
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-02-01 16:52:45 +0530

    Add signup page (#8)
    * Added background image
    
    * Added sign up page
    
    * Added styling in sign up page

---
+++ src/pages/signup.js
@@ -0,0 +1,23 @@
+import styles from '@/styles/Signup.module.css'
+export default function Signup() {
+    return <div className={styles.main}>
+        <div className={styles.signupContainer}>
+            <div className={styles.imageContainer}/>
+            <hr/>
+            <div className={styles.signup}>
+                <div className={styles.title}>
+                    Sign Up
+                </div>
+                <div className={styles.space}/>
+                <form title={"Signup"} className={styles.signupForm}>
+                    <input className={styles.inputHolder} title={"Email"} type={"text"} placeholder={"Name"}/>
+                    <input className={styles.inputHolder} title={"Email"} type={"email"} placeholder={"Email"}/>
+                    <input className={styles.inputHolder} title={"Password"} type={"password"} placeholder={"Password"}/>
+                    <button className={styles.button} >
+                        Submit
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div>
+}
