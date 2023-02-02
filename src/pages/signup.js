772aa5bb3e9d10051eabfe9e087e21084b7b4957
Shreyash Patil <113231121+Shreyash91466@users.noreply.github.com>
2023-02-02 21:10:21 +0530

    Changed email to name (#17)

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -10,7 +10,7 @@ export default function Signup() {
                 </div>
                 <div className={styles.space}/>
                 <form title={"Signup"} className={styles.signupForm}>
-                    <input className={styles.inputHolder} title={"Email"} type={"text"} placeholder={"Name"}/>
+                    <input className={styles.inputHolder} title={"Name"} type={"text"} placeholder={"Name"}/>
                     <input className={styles.inputHolder} title={"Email"} type={"email"} placeholder={"Email"}/>
                     <input className={styles.inputHolder} title={"Password"} type={"password"} placeholder={"Password"}/>
                     <button className={styles.button} >
