3e2db6e4f0a9e5039369564d3799af41e0b89819
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-02-03 12:59:22 +0530

    Changes in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,29 @@
+import Link from "next/link";
+import styles from "@/styles/Footer.module.css";
+
 export default function Footer() {
-    return <div>
-        Footer
+    return <div className={styles.main}>
+        <div className={styles.brand}>
+            <div className={styles.title}>
+                HRV-Mart
+            </div>
+            <div className={styles.copyright}>
+                Copyright © 2023 HRV-Mart Inc. All rights reserved.
+            </div>
+        </div>
+        <div className={styles.links}>
+            <Link href={"/"} className={styles.link}>
+                Home Page
+            </Link>
+            <Link href={"/about"} className={styles.link}>
+                About Us
+            </Link>
+            <Link href={"https://github.com/HRV-Mart/HRV-Mart/issues"} className={styles.link}>
+                Report any issue
+            </Link>
+            <Link href={"https://github.com/HRV-Mart/HRV-Mart"} className={styles.link}>
+                Contribute to website
+            </Link>
+        </div>
     </div>
 }
