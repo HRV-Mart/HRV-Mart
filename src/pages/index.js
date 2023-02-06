3000c6c684ab6b68782966c88e589a41bb5132b4
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-02-06 14:09:02 +0530

    Added products on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
 import {getRequest} from "@/service/network/network";
 import {logMessage} from "@/service/logging/logging";
+import Product from "@/components/product";
 
 export default function Home({products}) {
     logMessage(products);
@@ -14,7 +15,18 @@ export default function Home({products}) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        Home Page
+          <div className={styles.products}>
+              {
+                  products.map(function (product, index) {
+                      return <Product
+                          key={product.id}
+                          title={product.name}
+                          id={product.id}
+                          images={product.images}
+                      />
+                  })
+              }
+          </div>
       </main>
     </>
   )
@@ -24,7 +36,7 @@ export async function getServerSideProps({ req, res }) {
     if (response.status === 200) {
         return {
             props: {
-                products: response.data
+                products: response.data.data
             }
         }
     }
