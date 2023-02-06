8fbb4ce37560dbfd1dbe8e22e3a202681e2a1800
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-02-06 19:30:34 +0530

    Minor change

--- src/pages/api/product/index.js
+++ src/pages/api/product/index.js
@@ -5,5 +5,4 @@ export default async function handler(req, res) {
     const response = await getRequest(`${process.env.BACKEND_URL}/product`, {}, true);
     logMessage(response);
     res.status(response.status).json(response.data);
-
 }
