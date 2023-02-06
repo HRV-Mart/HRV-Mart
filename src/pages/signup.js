f9166f3e4c1b2251b7377a58cd34f7049d56ca44
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-02-06 19:19:46 +0530

    Integrated sign up with backend

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,5 +1,16 @@
 import styles from '@/styles/Signup.module.css'
+import {useState} from "react";
+import Link from "next/link";
+import {postRequest} from "@/service/network/network";
+import Router from "next/router";
+import {logError} from "@/service/logging/logging";
 export default function Signup() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [isLoading, setLoading] = useState(false);
+    const [messageCode, setMessageCode] = useState(0);
+
     return <div className={styles.main}>
         <div className={styles.signupContainer}>
             <div className={styles.imageContainer}/>
@@ -9,15 +20,99 @@ export default function Signup() {
                     Sign Up
                 </div>
                 <div className={styles.space}/>
-                <form title={"Signup"} className={styles.signupForm}>
-                    <input className={styles.inputHolder} title={"Name"} type={"text"} placeholder={"Name"}/>
-                    <input className={styles.inputHolder} title={"Email"} type={"email"} placeholder={"Email"}/>
-                    <input className={styles.inputHolder} title={"Password"} type={"password"} placeholder={"Password"}/>
-                    <button className={styles.button} >
+                <div title={"Signup"} className={styles.signupForm}>
+                    <input
+                        className={styles.inputHolder}
+                        title={"Name"}
+                        type={"text"}
+                        placeholder={"Name"}
+                        value={name}
+                        onChange={(event) => {setName(event.target.value)}}
+                    />
+                    <input
+                        className={styles.inputHolder}
+                        title={"Email"}
+                        type={"email"}
+                        placeholder={"Email"}
+                        value={email}
+                        onChange={(event) => {setEmail(event.target.value)}}
+                    />
+                    <input
+                        className={styles.inputHolder}
+                        title={"Password"}
+                        type={"password"}
+                        placeholder={"Password"}
+                        value={password}
+                        onChange={(event) => {setPassword(event.target.value)}}
+                    />
+                    <button className={styles.button} onClick={()=>{signup()}} >
                         Submit
                     </button>
-                </form>
+                    <div className={styles.loginMessage}>
+                        Do not have an account? {" "}
+                        <Link href={'/login'} className={styles.login}>
+                            Login
+                        </Link>
+                    </div>
+                    {generateMessage()}
+                </div>
             </div>
         </div>
     </div>
+
+    function generateMessage() {
+        if (isLoading === true) {
+            return <div className={styles.messageLoading}>
+                Loading ...
+            </div>
+        }
+        else if (messageCode === 0) {
+            return <div/>
+        }
+        else if (messageCode === 200) {
+            return <div className={styles.messageSuccess}>
+                Login Success fully
+            </div>
+        }
+        else if (messageCode === 404) {
+            return <div className={styles.messageError}>
+                Incorrect credentials
+            </div>
+        }
+        else {
+            return <div className={styles.messageError}>
+                Something went wrong
+            </div>
+        }
+    }
+    function signup() {
+        setLoading(true);
+        postRequest(
+            '/api/signup',
+            {
+                name: name,
+                email: email,
+                password: password
+            },
+            {
+                "Content-Type": "application/json"
+            },
+            true
+        )
+            .then((data)=> {
+                setLoading(false);
+                setMessageCode(data.status);
+
+                if (data.status === 200) {
+                    Router.push('/')
+                    // Save jwt token
+
+                }
+            })
+            .catch((error) => {
+                logError(error)
+                setMessageCode(500);
+                setLoading(false);
+            })
+    }
 }
