af5fa9ab51cab8a05a5dc8603ff081d61a350c1e
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-02-20 16:11:10 +0530

    Updated home page

--- src/components/product.js
+++ src/components/product.js
@@ -2,13 +2,12 @@ import styles from "@/styles/ProductOverview.module.css"
 import Link from "next/link";
 import Image from "next/image";
 export default function Product ({id, title, images}) {
-    return <Link href={`/product/${id}`}>
-        <div className={styles.main}>
-
-                <h2>
+    return <div className={styles.main}>
+            <Link href={`/product/${id}`}>
+                <h2 className={styles.title}>
                     {title}
                 </h2>
+            </Link>
             <Image className={styles.image} src={images[0]} alt={title} width={200} height={200} />
         </div>
-    </Link>
 }
