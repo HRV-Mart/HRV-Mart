6e3a4b903c1ce5b0e93fe5dc9826387be7e2ac8b
Harsh3305 <harshverma3305@gmail.com>
2023-04-08 18:24:27 +0530

    Change search bar

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,25 @@
 import styles from "../styles/Header.module.css";
 import Link from "next/link";
+import Typewriter from 'typewriter-effect';
 
 export default function Header() {
     return <div className={styles.main}>
         <Link  href={"/"} className={styles.icon}>
            HRV-Mart
         </Link>
-        <input className={styles.searchBar} placeholder="Search Products" title={"Search Products"}/>
+        <div className={styles.searchBar} title="Search Product">
+        <Typewriter options={{
+                strings: [
+                    "Search Product",
+                    "Search Bags",
+                    "Search Speeker",
+                    "Search T-Shirt",
+                ],
+                autoStart: true,
+                loop: true,
+
+            }}/>
+        </div>
         <div className={styles.info}>
             <Link href={"/login"}>
                 Login
