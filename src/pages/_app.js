588c8ac5f1a2f57f854a62257f33a4e1d1215851
Harsh3305 <harshverma3305@gmail.com>
2023-04-08 12:21:35 +0530

    Added toast container

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,15 @@
 import '@/styles/globals.css'
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+
 
 export default function App({ Component, pageProps }) {
   return <>
     <Header/>
     <Component {...pageProps} />
+    <ToastContainer/>
     <Footer/>
   </>
 
