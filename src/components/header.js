9b5c66d96531d0a4c0706a2a49839b890c77c331
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-05-04 21:21:32 +0530

    Update header (#90)
    * Fixed typo
    
    * Change search bar background color
    
    * Updated header. Added icon title and change icon animation

--- src/components/header.js
+++ src/components/header.js
@@ -19,8 +19,8 @@ export default function Header() {
                 strings: [
                     "Search Product",
                     "Search Bags",
-                    "Search Speakera",
-                    "Search T-Shirt",
+                    "Search Speaker",
+                    "Search T-Shirt"
                 ],
                 autoStart: true,
                 loop: true,
@@ -28,14 +28,17 @@ export default function Header() {
             }}/>
         </div>
         <div className={styles.info}>
-            <Link href={"/login"}>
-                <AiOutlineUser className={styles.icon} title={"Login"}/>
+            <Link href={"/login"} className={styles.icon_container}>
+                <AiOutlineUser  title={"Login"}/>
+                <div className={styles.iconTitle}>Login</div>
             </Link>
-            <Link href={"/like"}>
-                <AiOutlineHeart className={styles.icon} title={"Like"}/>
+            <Link href={"/like"} className={styles.icon_container}>
+                <AiOutlineHeart title={"Like"}/>
+                <div className={styles.iconTitle}>Like</div>
             </Link>
-            <Link href={"/cart"}>
-                <AiOutlineShoppingCart className={styles.icon} title={"Cart"}/>
+            <Link href={"/cart"} className={styles.icon_container}>
+                <AiOutlineShoppingCart title={"Cart"}/>
+                <div className={styles.iconTitle}>Cart</div>
             </Link>
         </div>
     </div>
