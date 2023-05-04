f5ff4005b79c7af39ba54fa1b60e983c171301c7
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-05-04 10:03:56 +0530

    Update header of application (#85)
    * Added dark theme in application
    
    * Added background color
    
    * Replace text with icons
    
    * Added logo of appplication
    
    * Added animation in icons

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,18 @@
 import styles from "../styles/Header.module.css";
 import Link from "next/link";
 import Typewriter from 'typewriter-effect';
+import {AiOutlineShoppingCart, AiOutlineHeart, AiOutlineUser} from "react-icons/ai";
+import Image from "next/image";
 
 export default function Header() {
     return <div className={styles.main}>
-        <Link  href={"/"} className={styles.icon}>
-           HRV-Mart
+        <Link  href={"/"} className={styles.logo}>
+            <Image
+                src={"/logo.png"}
+                height={40}
+                width={150}
+                alt={"HRV-Mart"}
+            />
         </Link>
         <div className={styles.searchBar} title="Search Product">
         <Typewriter options={{
@@ -22,10 +29,13 @@ export default function Header() {
         </div>
         <div className={styles.info}>
             <Link href={"/login"}>
-                Login
+                <AiOutlineUser className={styles.icon} title={"Login"}/>
+            </Link>
+            <Link href={"/like"}>
+                <AiOutlineHeart className={styles.icon} title={"Like"}/>
             </Link>
             <Link href={"/cart"}>
-                Cart
+                <AiOutlineShoppingCart className={styles.icon} title={"Cart"}/>
             </Link>
         </div>
     </div>
