0b4eae1570b8bf75fcc978d324d71b8a67154bbe
Harsh3305 <harshverma3305@gmail.com>
2023-05-08 19:14:12 +0530

    Remove logMessage

--- src/pages/api/product/index.js
+++ src/pages/api/product/index.js
@@ -1,5 +1,4 @@
 import {getRequest} from "@/service/network/network";
-import {logMessage} from "@/service/logging/logging";
 
 export default async function handler(req, res) {
     const response = await getRequest(`${process.env.BACKEND_URL}/product`, {}, true);
