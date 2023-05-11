f6306ab1c9dcf520051b16abc941589a422ab143
Harsh3305 <harshverma3305@gmail.com>
2023-05-11 18:38:36 +0530

    Like page completed

--- src/pages/like.js
+++ src/pages/like.js
@@ -1,47 +1,32 @@
-import styles from "@/styles/Like.module.css";
-import {getRequest} from "@/service/network/network";
-import {useEffect, useState} from "react";
-import Router from "next/router";
-import {logError, logMessage} from "@/service/logging/logging";
+import Product from "@/components/product";
+import { logMessage } from "@/service/logging/logging";
+import { getRequest } from "@/service/network/network";
+import styles from "@/styles/Like.module.css"
+import { useEffect, useState } from "react";
 
 export default function Like ({token, index}) {
-    if (! token) {
-        try {
-            Router.push("/404")
-                .then(()=>{})
-                .catch((error)=>{logError(error)});
-        }
-        catch (e) {
-            logError(e);
-        }
-    }
+
     const [products, setProducts] = useState([]);
-    async function loadData(page_index) {
-        const response = await getRequest(
-            `/api/like?page=${page_index}`,
-            token,
-            true
-        );
-        if (response.status === 200) {
-            setProducts(response.data.data);
-        }
-        else {
-            setProducts([]);
-        }
+    function getData() {
+        getRequest('/api/like', token, true)
+            .then((result) => {
+                setProducts(result.data.data);
+                logMessage(products);
+            })
     }
-    useEffect(() => {
-        loadData(index)
-    }, [])
+
+    useEffect(()=>{getData()}, [0]);
     return <div className={styles.main}>
-        <div >
-            {
-                products.map(function (product, index) {
-                    return <div>
-                        {product.productId}
-                    </div>
-                })
-            }
-        </div>
+        {
+            products.map((product, index)=> {
+                return <Product
+                    key={product.id}
+                    title={product.name}
+                    id={product.id}
+                    images={product.images}
+                />
+            })
+        }
     </div>
 }
 export async function getServerSideProps(context) {
