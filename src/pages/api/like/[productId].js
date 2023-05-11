76061e11cf1cc392dfaada018926efc20e34dd64
Harsh3305 <harshverma3305@gmail.com>
2023-05-11 20:45:12 +0530

    Updated api

--- src/pages/api/like/[productId].js
+++ src/pages/api/like/[productId].js
@@ -1,9 +1,17 @@
-import { deleteRequest} from "@/service/network/network";
+import { deleteRequest, getRequest} from "@/service/network/network";
 
 export default async function handler(req, res) {
     const authentication = req.headers.authentication;
     const token = authentication.split(":")[1];
     switch (req.method) {
+        case "GET":
+            const get_response = await getRequest(
+                `${process.env.BACKEND_URL}/like/${req.query.productId}`,
+                token,
+                false
+            );
+            res.status(get_response.status).send(get_response.data);
+            break;
         case "DELETE":
             const delete_response = await deleteRequest(
                 `${process.env.BACKEND_URL}/like/${req.query.productId}`,
