645f28cf4af7c734fd1f0180dacc439ae27437de
Harsh3305 <harshverma3305@gmail.com>
2023-06-03 20:07:34 +0530

    Updated product page design

--- src/pages/product/[productId].js
+++ src/pages/product/[productId].js
@@ -1,36 +1,38 @@
 import { logError, logMessage } from "@/service/logging/logging";
-import {deleteRequest, getRequest, postRequest, putRequest} from "@/service/network/network";
+import { deleteRequest, getRequest, postRequest, putRequest } from "@/service/network/network";
 import styles from "@/styles/Product.module.css";
-import {useEffect, useState} from "react";
-import {AiFillHeart} from "react-icons/ai";
+import { useEffect, useState } from "react";
+import { AiFillHeart } from "react-icons/ai";
 import { toast } from "react-toastify";
 
-export default function ProductPage ({product, token}) {
+export default function ProductPage({ product, token }) {
     const [imageIndex, setImageIndex] = useState(0);
     const [totalItem, setTotalItem] = useState(0);
     const [isLike, setIsLike] = useState(false);
 
     useEffect(loadQuantity, [token, product.id])
 
-    useEffect(()=>{
+    useEffect(() => {
         isProductLiked();
     }, [0])
     function incrementCartItem(isNew) {
-        updateProductQuantity(totalItem+1, isNew)
-        setTotalItem(totalItem+1);
+        updateProductQuantity(totalItem + 1, isNew)
+        setTotalItem(totalItem + 1);
     }
     function decreaseCartItem() {
         if (totalItem >= 1) {
-            updateProductQuantity(totalItem-1, false)
-            setTotalItem(totalItem-1);
+            updateProductQuantity(totalItem - 1, false)
+            setTotalItem(totalItem - 1);
         }
     }
     function loadQuantity() {
         getRequest(`/api/cart/${product.id}`, token, true)
-            .then((data)=>{if (data.status === 200) {
-                setTotalItem(data.data)
-            }})
-            .catch((error)=>{logError(error); setTotalItem(0)})
+            .then((data) => {
+                if (data.status === 200) {
+                    setTotalItem(data.data)
+                }
+            })
+            .catch((error) => { logError(error); setTotalItem(0) })
     }
     function updateProductQuantity(updatedQuantity, isNew) {
         if (isNew) {
@@ -40,7 +42,7 @@ export default function ProductPage ({product, token}) {
                     productId: product.id,
                     quantity: updatedQuantity
                 },
-                {authentication: `bearer:${token}`, "Content-Type": "application/json"},
+                { authentication: `bearer:${token}`, "Content-Type": "application/json" },
                 false
             )
                 .then(logMessage)
@@ -53,7 +55,7 @@ export default function ProductPage ({product, token}) {
                     productId: product.id,
                     quantity: updatedQuantity
                 },
-                {authentication: `bearer:${token}`, "Content-Type": "application/json"},
+                { authentication: `bearer:${token}`, "Content-Type": "application/json" },
                 false
             )
                 .then(logMessage)
@@ -63,7 +65,7 @@ export default function ProductPage ({product, token}) {
             deleteRequest(
                 `/api/cart/${product.id}`,
                 {},
-                {authentication: `bearer:${token}`},
+                { authentication: `bearer:${token}` },
                 false
             )
                 .then(logMessage)
@@ -79,83 +81,87 @@ export default function ProductPage ({product, token}) {
             />
             <div className={styles.imageIndicatorContainer}>
                 <button
-                    onClick={()=>decrementImageIndex()}
+                    onClick={() => decrementImageIndex()}
                     className={styles.imageIndicator}
                 >
                     ◄
                 </button>
                 <div className={styles.textIndicator}>
-                    {imageIndex+1}/{product.images.length}
+                    {imageIndex + 1}/{product.images.length}
                 </div>
                 <button
-                    onClick={()=>incrementImageIndex()}
+                    onClick={() => incrementImageIndex()}
                     className={styles.imageIndicator}
                 >
                     ►
                 </button>
             </div>
         </div>
-        <hr className={styles.vertical_divider}/>
-        <div className={styles.details}>
-            <div className={styles.title}>
-                {product.name}
-            </div>
-            <div className={styles.descriptionContainer}>
-                <div className={styles.descriptionTitle}>
-                    Description:
-                </div>
-                <div className={styles.description}>
-                    {product.description}
-                </div>
-            </div>
-            <div className={styles.descriptionContainer}>
-                <div className={styles.descriptionTitle}>
-                    Price:
-                </div>
-                <div className={styles.description}>
-                    ₹ {product.price}
-                </div>
-            </div>
-            {
-                token ? <div className={styles.likeCartContainer}>
-                    <div className={styles.likeContainer} onClick={changeLike}>
-                        {
-                            isLike ?
-                                <AiFillHeart
-                                    fill={"red"}/> :
-                                <AiFillHeart  />
-                        }
+        <hr className={styles.vertical_divider} />
+        <div className={styles.rightContainer}>
+            <div className={styles.upperContainer}>
+                <div className={styles.details}>
+                    <div className={styles.title}>
+                        {product.name}
                     </div>
-                    <div className={styles.cartContainer}>
-                        {cartHolder()}
+                    <div className={styles.descriptionContainer}>
+                        <div className={styles.descriptionTitle}>
+                            Description:
+                        </div>
+                        <div className={styles.description}>
+                            {product.description}
+                        </div>
                     </div>
-                </div> : 
-                <></>
-            }
-            <hr className={styles.horizontal_divider}/>
+                    <div className={styles.descriptionContainer}>
+                        <div className={styles.descriptionTitle}>
+                            Price:
+                        </div>
+                        <div className={styles.description}>
+                            ₹ {product.price}
+                        </div>
+                    </div>
+                    {
+                        token ? <div className={styles.likeCartContainer}>
+                            <div className={styles.likeContainer} onClick={changeLike}>
+                                {
+                                    isLike ?
+                                        <AiFillHeart
+                                            fill={"red"} /> :
+                                        <AiFillHeart />
+                                }
+                            </div>
+                            <div className={styles.cartContainer}>
+                                {cartHolder()}
+                            </div>
+                        </div> :
+                            <></>
+                    }
+                    <hr className={styles.horizontal_divider} />
+                </div>
+            </div>
         </div>
     </div>
     function changeLike() {
-        if (! isLike) {
+        if (!isLike) {
             postRequest(`/api/like`, {
                 productId: product.id
-            }, {authentication: `bearer:${token}`, "Content-Type": "application/json"}, false).then(()=>{
-                toast('Product added to Like', { hideProgressBar: false, autoClose: 2000, type: 'success', theme: "light"});
-            }).catch((error)=>{
-                toast('Error while processing your request', { hideProgressBar: false, autoClose: 2000, type: 'error', theme: "light"});
+            }, { authentication: `bearer:${token}`, "Content-Type": "application/json" }, false).then(() => {
+                toast('Product added to Like', { hideProgressBar: false, autoClose: 2000, type: 'success', theme: "light" });
+            }).catch((error) => {
+                toast('Error while processing your request', { hideProgressBar: false, autoClose: 2000, type: 'error', theme: "light" });
             });
         }
         else {
             deleteRequest(
                 `/api/like/${product.id}`,
                 {},
-                {authentication: `bearer:${token}`},
+                { authentication: `bearer:${token}` },
                 false
-            ).then((result)=>{
+            ).then((result) => {
                 logMessage(result)
-                toast('Product remove from Like', { hideProgressBar: false, autoClose: 2000, type: 'info', theme: "light"});
-            }).catch((error)=>{
-                toast('Error while processing your request', { hideProgressBar: false, autoClose: 2000, type: 'error', theme: "light"});
+                toast('Product remove from Like', { hideProgressBar: false, autoClose: 2000, type: 'info', theme: "light" });
+            }).catch((error) => {
+                toast('Error while processing your request', { hideProgressBar: false, autoClose: 2000, type: 'error', theme: "light" });
             });
 
             // deleteRequest(
@@ -166,7 +172,7 @@ export default function ProductPage ({product, token}) {
             // )
             // .then(logMessage)
             // .catch(logMessage)
-            
+
         }
         setIsLike(!isLike)
     }
@@ -174,20 +180,20 @@ export default function ProductPage ({product, token}) {
         if (totalItem === 0 || totalItem == 0) {
             return <div
                 className={styles.cartHolder}
-                onClick={()=>{incrementCartItem(true)}}
+                onClick={() => { incrementCartItem(true) }}
             >
                 ADD TO CART
             </div>
         }
         else {
             return <div className={styles.cartHolder}>
-                <div onClick={()=>{decreaseCartItem()}} className={`${styles.cartButton} ${styles.red_background}`}>
+                <div onClick={() => { decreaseCartItem() }} className={`${styles.cartButton} ${styles.red_background}`}>
                     -
                 </div>
                 <div className={styles.quantity}>
                     {totalItem}
                 </div>
-                <div onClick={()=>{incrementCartItem(false)}} className={`${styles.cartButton} ${styles.green_background}`}>
+                <div onClick={() => { incrementCartItem(false) }} className={`${styles.cartButton} ${styles.green_background}`}>
                     +
                 </div>
             </div>
@@ -196,38 +202,38 @@ export default function ProductPage ({product, token}) {
 
     function incrementImageIndex() {
         const totalImageSize = product.images.length;
-        if (imageIndex >= totalImageSize-1) {
+        if (imageIndex >= totalImageSize - 1) {
             setImageIndex(0)
         }
         else {
-            setImageIndex(imageIndex+1)
+            setImageIndex(imageIndex + 1)
         }
     }
     function decrementImageIndex() {
         const totalImageSize = product.images.length;
         if (imageIndex <= 0) {
-            setImageIndex(totalImageSize-1)
+            setImageIndex(totalImageSize - 1)
         }
         else {
-            setImageIndex(imageIndex-1)
+            setImageIndex(imageIndex - 1)
         }
     }
     function isProductLiked() {
         getRequest(`/api/like/${product.id}`, token, false)
-        .then((data) => {
-            logMessage(data.data);
-            if (data.status === 200) {
-                if (data.data === "true" || data.data === true) {
-                    setIsLike(true)
+            .then((data) => {
+                logMessage(data.data);
+                if (data.status === 200) {
+                    if (data.data === "true" || data.data === true) {
+                        setIsLike(true)
+                    }
+                    else {
+                        setIsLike(false)
+                    }
                 }
-                else {
-                    setIsLike(false)
-                }
-            }
-        })
-        .catch((error)=>{
-            logError(error);
-        })
+            })
+            .catch((error) => {
+                logError(error);
+            })
     }
 }
 export async function getServerSideProps(content) {
