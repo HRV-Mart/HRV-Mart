653ba29dbeca4fbde7adb02e8ad53dbe44f419a0
Harsh3305 <harshverma3305@gmail.com>
2023-06-05 09:27:58 +0530

    Changes in reviews

--- src/components/review.js
+++ src/components/review.js
@@ -1,22 +1,31 @@
 import styles from "@/styles/review.module.css";
-import {AiOutlineUser } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
 
-export default function Review({user, review}) {
+export default function Review({ user, review }) {
     return <div className={styles.main}>
-        <div className={styles.user}>
-            <AiOutlineUser/>
-            <div>
-                {user.name}
+        <div className={styles.upper}>
+            <div className={styles.user}>
+                <AiOutlineUser />
+                <div>
+                    {user.name}
+                </div>
+            </div>
+            <hr />
+            <div className={styles.review}>
+                <div className={styles.title}>
+                    {review.title}
+                </div>
+                <div className={styles.description}>
+                    {review.description}
+                </div>
             </div>
         </div>
-        <hr/>
-        <div className={styles.review}>
-            <div className={styles.title}>
-                {review.title}
-            </div>
-            <div className={styles.description}>
-                {review.description}
-            </div>
+        <div className={styles.lower}>
+            {
+                review.images.map((image, index)=>{
+                    return <img src={image} key={index} className={styles.image}/>
+                })
+            }
         </div>
     </div>
 }
