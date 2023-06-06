acb726a4e5ad3c2dbf8f93e446410bb5198e29ff
Harsh3305 <harshverma3305@gmail.com>
2023-06-06 16:05:00 +0530

    Minor changes in api

--- src/pages/api/product/index.js
+++ src/pages/api/product/index.js
@@ -1,6 +1,7 @@
-import {getRequest} from "@/service/network/network";
+import {getQueryFromURL, getRequest} from "@/service/network/network";
 
 export default async function handler(req, res) {
-    const response = await getRequest(`${process.env.BACKEND_URL}/product`, {}, true);
+    const query = getQueryFromURL(req.url, 'api/product');
+    const response = await getRequest(`${process.env.BACKEND_URL}/product${query}`, {}, true);
     res.status(response.status).json(response.data);
 }
