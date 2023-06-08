fc77e0da7f616cbead5b5acf7ef0aa5f9473979f
Harsh3305 <harshverma3305@gmail.com>
2023-06-08 14:52:49 +0530

    Updated _app file to load token from appwrite at the load of every page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,16 +4,34 @@ import Footer from "@/components/footer";
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 import {useState} from "react";
-
+import { logMessage } from '@/service/logging/logging';
+import { Account, Client } from 'appwrite';
 
 export default function App({ Component, pageProps }) {
+  const appwrite_endpoint = process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT;
+  const appwrite_project = process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID;
+  const application_url = process.env.NEXT_PUBLIC_APPLICATION_URL;
+
+  const client = new Client()
+  .setEndpoint(appwrite_endpoint)
+  .setProject(appwrite_project);
+
+  const account = new Account(client)
+  const promise = account.createJWT();
+
+  promise.then(function (response) {
+    setToken(response.jwt)
+  }, function (error) {
+    setToken(null)
+  });
+
   const [token, setToken] = useState(null);
   setTimeout(()=>{
     setToken(null)
   }, 1000*60*10);
   return <>
     <Header token={token}/>
-    <Component {...pageProps} token={token} setToken={setToken}/>
+    <Component {...pageProps} token={token} account={account} application_url={application_url}/>
     <ToastContainer/>
     <Footer/>
   </>
