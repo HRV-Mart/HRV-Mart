b0964555695102f402a6e439f66364007f145f97
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-06-10 12:22:30 +0530

    Update app.js file (#140)
    * Update login and remove sign up api
    
    * Updated app
    
    * FIXED: excess api call

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,44 +3,67 @@ import Header from "@/components/header";
 import Footer from "@/components/footer";
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
-import {useState} from "react";
-import { logMessage } from '@/service/logging/logging';
+import { useEffect, useState } from "react";
 import { Account, Client } from 'appwrite';
+import { postRequest } from '@/service/network/network';
+import { logError, logMessage } from '@/service/logging/logging';
 
 export default function App({ Component, pageProps }) {
   const appwrite_endpoint = process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT;
   const appwrite_project = process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID;
   const application_url = process.env.NEXT_PUBLIC_APPLICATION_URL;
 
-  var account = null
-
-  if (appwrite_endpoint && appwrite_project && application_url) {
-    const client = new Client()
-    .setEndpoint(appwrite_endpoint)
-    .setProject(appwrite_project);
+  const [details, setDetails] = useState({
+    account: null,
+    token: null
+  })
   
-    account = new Account(client)
-    const promise = account.createJWT();
-  
-    promise.then(function (response) {
-      setToken(response.jwt)
-    }, function (error) {
-      setToken(null)
-    });
-  }
-  else {
-    account = null;
-  }
+  useEffect(()=>{
+    generateToken().then().catch(logError);
+    setInterval(generateToken, 1000*60*10);
+  }, [])
 
-
-  const [token, setToken] = useState(null);
-  setTimeout(()=>{
-    setToken(null)
-  }, 1000*60*10);
   return <>
-    <Header token={token}/>
-    <Component {...pageProps} token={token} account={account} application_url={application_url}/>
-    <ToastContainer/>
-    <Footer/>
+    <Header token={details.token} />
+    <Component {...pageProps} token={details.token} account={details.account} application_url={application_url} />
+    <ToastContainer />
+    <Footer />
   </>
+
+  async function generateToken() {
+    logMessage("TICK TOCK");
+
+    const client = new Client()
+    client.setEndpoint(appwrite_endpoint)
+    client.setProject(appwrite_project)
+
+    const account = new Account(client)
+    var token = null
+
+    try {
+      const response = await account.createJWT();
+      const jwt = response.jwt;
+
+      const result = await postRequest(
+        '/api/login',
+        {
+          jwt: jwt
+        },
+        {
+          "Content-Type": "application/json"
+        },
+        true
+      )
+
+      token = result.data.token
+    }
+    catch (error) {
+      logError(error)
+    }
+    
+    setDetails({
+      account: account,
+      token: token
+    })
+  }
 }
